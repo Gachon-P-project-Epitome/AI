829bb39c5f95c0c6cee5b1163c664576291082cd
iwantbear <gyuhanhwang@gmail.com>
2024-12-06 18:14:19 +0900

    feat simple edit

--- DenseNet/ForDATA/Convert_FMA_16kHz_2_Images_ECHO_ECHONOISE.py
+++ DenseNet/ForDATA/Convert_FMA_16kHz_2_Images_ECHO_ECHONOISE.py
@@ -35,11 +35,19 @@ if not os.path.exists(image_dir):
     os.makedirs(image_dir)
 i = 0                       # Number of sound files
 for g in genres:
-    for k, filename in enumerate(os.listdir(f'/home/hwang-gyuhan/Workspace/dataset/fma_wav_pcm_echo/{g}')):
-        songname = f'/home/hwang-gyuhan/Workspace/dataset/fma_wav_pcm_echo/{g}/{filename}'
+    for k, filename in enumerate(os.listdir(f'/home/hwang-gyuhan/Workspace/dataset/Spotify_wav_pcm_echo/{g}')):
+        songname = f'/home/hwang-gyuhan/Workspace/dataset/Spotify_wav_pcm_echo/{g}/{filename}'
+        
+        y, sr = librosa.load(songname, mono=True, sr=fs)  # Downsampling to 16 kHz
+        duration = librosa.get_duration(y=y, sr=sr)  # 오디오 길이 계산
+        
+        # ADD: 29.35초 이상인 파일만 선택
+        if duration < 29.35:
+            continue  # 29.35초 미만 파일은 건너뜀
+        
         print(songname.ljust(85), "  i =  ", i, "/", total_file)
         filename3 = filename.split("_")[0] + "_PCM16_ECHO.wav"
-        songname3= f'/home/hwang-gyuhan/Workspace/dataset/fma_wav_pcm_echo/{g}/{filename3}'
+        songname3= f'/home/hwang-gyuhan/Workspace/dataset/Spotify_wav_pcm_echo/{g}/{filename3}'
         file_name3 = image_dir + filename3.split("_")[0] +"_ECHO.png"
         file_name4 = image_dir + filename3.split("_")[0] +"_ECHO_NOISE.png"
         print ("songname3 = ", songname3)

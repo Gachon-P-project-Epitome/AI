6dc2ef09907be5e78487cd2bdf421eccb12936b2
iwantbear <gyuhanhwang@gmail.com>
2024-12-21 13:49:08 +0900

    feag: TestCode update

--- DenseNet/TestCode/Preprocessing.py
+++ DenseNet/TestCode/Preprocessing.py
@@ -1,4 +1,3 @@
-import io
 import subprocess
 import librosa
 import soundfile as sf
@@ -6,30 +5,23 @@ import numpy as np
 
 
 class Preprocessing:
+    
     def __init__(self, sr=16000):
         self.sr = sr
-
-    def wav_convert(self, mp3_path):
-        temp_wav = io.BytesIO()
-        cmd = f"ffmpeg -hide_banner -loglevel panic -y -i \"{mp3_path}\" -f wav pipe:1"
-        process = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-        temp_wav.write(process.stdout)
-        temp_wav.seek(0)
-        data, sr = librosa.load(temp_wav, sr=self.sr, mono=True)
-        print(f"Successfully converted MP3 to WAV (in memory): {mp3_path}")
-        return data, sr
-
-    def pcm_convert(self, wav_data, samplerate):
-        pcm_buffer = io.BytesIO()
-        sf.write(pcm_buffer, wav_data, samplerate, format='WAV', subtype='PCM_16')
-        pcm_buffer.seek(0)
-        print("Successfully converted WAV to PCM16 (in memory)")
-        return pcm_buffer
-
-    def process_audio(self, mp3_path):
-        wav_data, samplerate = self.wav_convert(mp3_path)  # MP3 -> WAV 변환
-        if wav_data is not None:
-            pcm_data = self.pcm_convert(wav_data, samplerate)  # WAV -> PCM 변환
-            return pcm_data
+    
+    def wav_convert(self, mp3_path, output_wav_path):
+        """Converts MP3 to WAV and saves it to disk."""
+        cmd = f"ffmpeg -hide_banner -loglevel panic -y -i \"{mp3_path}\" -f wav \"{output_wav_path}\""
+        subprocess.run(cmd, shell=True, check=True)
+        print(f"Successfully converted MP3 to WAV: {mp3_path} -> {output_wav_path}")
         
-        return None
+    def pcm_convert(self, wav_path, output_pcm_path):
+        """Converts WAV to PCM and saves it to disk."""
+        wav_data, sr = librosa.load(wav_path, sr=self.sr, mono=True)
+        sf.write(output_pcm_path, wav_data, sr, format='WAV', subtype='PCM_16')
+        print(f"Successfully converted WAV to PCM16: {wav_path} -> {output_pcm_path}")
+    
+    def process_audio(self, mp3_path, wav_path, pcm_path):
+        """Processes MP3 file by converting to WAV and then to PCM."""
+        self.wav_convert(mp3_path, wav_path) 
+        self.pcm_convert(wav_path, pcm_path)  
